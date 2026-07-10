ed707fcb755d0d6880465b597a6dae902988a340
WimansaSamudinee <nadeerasamudinee@gmail.com>
2026-07-10 20:41:02 +0530

    feat : Add routing with layouts and route constants

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -1,5 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import PublicLayout from "../layouts/PublicLayout";
+import DashboardLayout from "../layouts/DashboardLayout";
+
 import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Dashboard from "../pages/Dashboard";
+import NotFound from "../pages/NotFound";
 
 export default function AppRouter() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route element={<PublicLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+
+        <Route element={<DashboardLayout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
