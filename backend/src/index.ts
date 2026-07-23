6c8f94c87ef58829037e0fc8be8649d9e4fe9eec
WimansaSamudinee <nadeerasamudinee@gmail.com>
2026-07-23 19:40:26 +0530

    feat : Configure backend to deploy on Vercel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -31,3 +31,5 @@ const port = process.env.PORT ? Number(process.env.PORT) : 4000;
 app.listen(port, () => {
   console.log(`Backend server listening on http://localhost:${port}`);
 });
+
+export default app;
